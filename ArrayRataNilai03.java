cc475d2db1b02c97c865867cdc331b858a6dfbcf
“aretaneilaputri <retaaneila@gmail.com>
2025-11-11 21:53:21 +0700

    Modifikasi Percobaan 3 (Pertanyaan nomor 1)

--- ArrayRataNilai03.java
+++ ArrayRataNilai03.java
@@ -7,6 +7,7 @@ public class ArrayRataNilai03 {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int jumlahLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
@@ -15,9 +16,13 @@ public class ArrayRataNilai03 {
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                jumlahLulus++;
+            }
         }
 
         rata2 = total / nilaiMhs.length;
         System.out.println("Rata-rata nilai = " + rata2);
+        System.out.println("Jumlah mahasiswa yang lulus = " + jumlahLulus);
     }
 }
