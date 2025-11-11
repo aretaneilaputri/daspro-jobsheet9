5871c5422c199bd90752c1e8eb7e659541e271f1
“aretaneilaputri <retaaneila@gmail.com>
2025-11-11 19:25:23 +0700

    Modifikasi (Pertanyaan nomor 3)

--- ArrayBilangan03.java
+++ ArrayBilangan03.java
@@ -1,13 +1,12 @@
 public class ArrayBilangan03 {
     public static void main(String[] args) {
         int[] bil = {5, 13, -7, 17};
-        
+
  
-        System.out.println(bil [0]);
-        System.out.println(bil [1]);
-        System.out.println(bil [2]);
-        System.out.println(bil [3]);
-    
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+
+            }
         }
     }
 
