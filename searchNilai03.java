a51d184c9b8bebccb40615a2dd985158427091ea
“aretaneilaputri <retaaneila@gmail.com>
2025-11-11 23:08:41 +0700

    Modifikasi Percobaan 4 (Pertanyaan nomor 3)

--- searchNilai03.java
+++ searchNilai03.java
@@ -29,7 +29,7 @@ public class searchNilai03 {
         if (hasil != -1) {
             System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
         } else {
-            System.out.println("Nilai " + key + " tidak ditemukan dalam data mahasiswa.");
+            System.out.println("Nilai yang dicari tidak ditemukan");
         }
 
     }
