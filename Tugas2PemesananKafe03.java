7ef6c630f13e4b5912dd3aceb3be4b715f6a7df6
“aretaneilaputri <retaaneila@gmail.com>
2025-11-12 10:22:02 +0700

    Tugas 2: Pesanan Makanan dan Minuman di kafe

---
+++ Tugas2PemesananKafe03.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Tugas2PemesananKafe03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah pesanan: ");
+        int jumlahPesanan = sc.nextInt();
+        sc.nextLine(); 
+
+        String[] namaPesanan = new String[jumlahPesanan];
+        int[] hargaPesanan = new int[jumlahPesanan];
+        int totalBiaya = 0;
+
+        for (int i = 0; i < jumlahPesanan; i++) {
+            System.out.println("\nPesanan ke-" + (i + 1));
+            System.out.print("Masukkan nama makanan/minuman: ");
+            namaPesanan[i] = sc.nextLine();
+            System.out.print("Masukkan harga Rp: ");
+            hargaPesanan[i] = sc.nextInt();
+            sc.nextLine();
+
+            totalBiaya += hargaPesanan[i];
+        }
+
+        System.out.println("\n=== DAFTAR PESANAN KAFE ===");
+        for (int i = 0; i < jumlahPesanan; i++) {
+            System.out.println((i + 1) + ". " + namaPesanan[i] + " - Rp " + hargaPesanan[i]);
+        }
+
+        System.out.println("---------------------------------");
+        System.out.println("Total biaya pesanan: Rp " + totalBiaya);
+    }
+}
